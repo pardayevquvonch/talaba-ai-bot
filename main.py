19a853edb168c06fb8117fd15e20f764e3e6463f
pardayevquvonch <pardayevquvonch486@gmail.com>
2026-08-27 13:19:30 +0500

    Update main.py

--- main.py
+++ main.py
@@ -2,16 +2,15 @@ import os
 import telebot
 from google import genai
 
-# Render yoki muhitdan token va kalitni avtomatik o'qiydigan qilib qo'yamiz
-BOT_TOKEN = os.environ.get('BOT_TOKEN', '8935762970:AAHOJcuRmBeNd2Up4ndLVdPgjf8dZzGyolc')
-GEMINI_KEY = os.environ.get('GEMINI_KEY', 'AQ.Ab8RN6KO1E_yOTeqIp85Llz0aEge6oiKEpv2D0fkn5mqFiD_yA')
+BOT_TOKEN = "8935762970:AAHOJcuRmBeNd2Up4NdLVdPgjf8dzZgyolc"
+GEMINI_KEY = "AQ.Ab8RN6JdNLQgKxCCDYANlz_6c1gnh4QAx-43MTWlMwkVVEOcBQ"
 
 bot = telebot.TeleBot(BOT_TOKEN)
 client = genai.Client(api_key=GEMINI_KEY)
 
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
-    bot.reply_to(message, "Assalomu alaykum! Slayd yoki referat uchun savolingizni yuboring.")
+    bot.reply_to(message, "Assalomu alaykum! Slayd va referatlar uchun savolingizni yuboring.")
 
 @bot.message_handler(func=lambda message: True)
 def handle_message(message):
